49c7c7db48c288085eecd6b7f1c2f8893eba618a
WADAAA2020 <87211924+WADAAA2020@users.noreply.github.com>
2021-10-05 21:25:56 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ function CarregarDoramas(){
     //array --> variável com subdivisões (índices)
     let doramas = ["img/squid-game.jpg","img/alice-in-borderland.jpg","img/extra-curricular.jpg","img/girl-from.jpg","img/sweet-home.jpg","img/itaewon-class.jpg"]
 
+    let totalDoramas = doramas.length
+
     //laço de repetição
-    for(let controle = 0 ; controle < 6 ; controle = controle + 1){
-        document.querySelector(".lista-doramas").innerHTML += "<img src="+ doramas [controle] + ">"
+    for(let i = 0 ; i < totalDoramas ; i++){
+        document.querySelector(".lista-doramas").innerHTML += "<img src="+ doramas[i] + ">"
     }
 }
